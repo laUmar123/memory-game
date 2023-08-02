cfcee6c491d487d994980238847c5d4e038099b7
Umar <work.umar7@hotmail.com>
2023-08-02 19:50:08 +0100

    created the Characters.js file that contains an array of all the characters that would be printed onto the screen, along with relevant information such as id, img link and whether it has been clicked or not

---
+++ src/styles/Characters.js
@@ -0,0 +1,88 @@
+const characters = [
+    {
+        name: 'Eren Yaeger',
+        img: '../public/eren.webp',
+        id: 1,
+        clicked: false
+    },
+    {
+        name: 'Mikasa Ackermann',
+        img: '../public/mikasa.webp',
+        id: 2,
+        clicked: false
+    },
+    {
+        name: 'Armin Arlelt',
+        img: '../public/armin.webp',
+        id: 3,
+        clicked: false
+    },
+    {
+        name: 'Levi Ackermann',
+        img: '../public/levi.webp',
+        id: 4,
+        clicked: false
+    },
+    {
+        name: 'Erwin Smith',
+        img: '../public/erwin.webp',
+        id: 5,
+        clicked: false
+    },
+    {
+        name: 'Hange Zoë',
+        img: '../public/hange.webp',
+        id: 6,
+        clicked: false
+    },
+    {
+        name: 'Jean Kirschtein',
+        img: '../public/jean.webp',
+        id: 7,
+        clicked: false
+    },
+    {
+        name: 'Conny Springer',
+        img: '../public/conny.webp',
+        id: 8,
+        clicked: false
+    },
+    {
+        name: 'Sasha Braus',
+        img: '../public/sasha.webp',
+        id: 9,
+        clicked: false
+    },
+    {
+        name: 'Moblit Berner',
+        img: '../public/moblit.webp',
+        id: 10,
+        clicked: false
+    },
+    {
+        name: 'Marlo Freudenberg',
+        img: '../public/marlo.webp',
+        id: 11,
+        clicked: false
+    },
+    {
+        name: 'Kenny Ackermann',
+        img: '../public/kenny.webp',
+        id: 12,
+        clicked: false
+    },
+    {
+        name: 'Hitch Dreyse',
+        img: '../public/hitch.webp',
+        id: 13,
+        clicked: false
+    },
+    {
+        name: 'Dot Pyxis',
+        img: '../public/pyxis.webp',
+        id: 14,
+        clicked: false
+    },
+]
+
+export { characters }
