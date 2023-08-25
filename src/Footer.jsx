b99190bb5d528ba531f95476da25243268ccd271
Umar <work.umar7@hotmail.com>
2023-08-25 20:50:13 +0100

    added the Footer.jsx component, and added the styling for it in footer.css

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import { AiFillGithub } from 'react-icons/ai'
+import './styles/footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Developed by <AiFillGithub />laUmar123</p>
+        </footer>
+    )
+}
