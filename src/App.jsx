116c14f594ea2f48cbe720580dfdf214a8bda17e
Umar <work.umar7@hotmail.com>
2023-09-24 23:49:25 +0100

    added the required states and useEffect's to the App component, also added the styling for the main game

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-function App() {
+import Header from "./Header"
+import Homepage from "./Homepage"
+import Footer from "./Footer"
+import React from "react"
+import Game from "./Game"
+import Card from "./Card"
+import { characters } from "./Characters"
 
+function App() {
+    const [showHeader, setShowHeader] = React.useState(false)
+    const [showHomepage, setShowHomepage] = React.useState(true)
+    const [showGame, setShowGame] = React.useState(false)
+    const [score, setScore] = React.useState(0)
+    const [bestScore, setBestScore] = React.useState(0)
+    const [allCharacters, setAllCharacters] = React.useState(characters)
+
+    React.useEffect(() => {
+        if (localStorage.getItem('bestScore')) {
+            setBestScore(localStorage.getItem('bestScore'))
+        }
+    }, [])
+
+    React.useEffect(() => {
+        if (localStorage.getItem('bestScore') && score >= localStorage.getItem('bestScore')) {
+            localStorage.setItem('bestScore', score)
+        } else if (localStorage.getItem('bestScore') && bestScore > localStorage.getItem('bestScore')) {
+            localStorage.setItem('bestScore', bestScore)
+        }
+    }, [score, bestScore])
 }
 
 export default App
